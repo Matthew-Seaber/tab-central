dd0c88ae11c9c7334fc9760fe81c2bf116f0434c
Matthew <matthewseaber@outlook.com>
2026-08-29 15:08:26 +0100

    Created basic UI for the sign up page

---
+++ app/signup/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useTheme } from "next-themes";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { ArrowLeft, Moon, Sun } from "lucide-react";
+
+export default function SignupPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <div className="flex flex-col min-h-screen items-center gap-8 bg-[radial-gradient(circle_at_50%_35%,#e9edff_0%,#f5f5ff_40%,#ffffff_80%)] dark:bg-[radial-gradient(circle_at_50%_35%,#1e2440_0%,#11131f_40%,#09090b_80%)]">
+      <nav className="flex w-full items-center justify-between p-6">
+        <Button
+          variant="outline"
+          size="icon"
+          className="p-6"
+          onClick={() => router.push("/settings")}
+        >
+          <ArrowLeft className="size-6 text-foreground/95" />
+        </Button>
+
+        <Button
+          variant="outline"
+          size="icon"
+          className="p-6"
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          {theme === "dark" ? (
+            <Sun className="size-6 text-foreground/95" />
+          ) : (
+            <Moon className="size-6 text-foreground/95" />
+          )}
+        </Button>
+      </nav>
+
+      <Card className="min-w-xs">
+        <CardHeader>
+          <CardTitle className="mb-2 text-lg">Sign up</CardTitle>
+          <CardDescription>
+            Create a Tab Central account to customise your new tab page and save
+            settings across devices
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-6">
+          <Field>
+            <FieldLabel htmlFor="username">Username</FieldLabel>
+            <Input
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Field>
+
+          <Field>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Input
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Field>
+        </CardContent>
+
+        <CardFooter>
+          <Button type="submit" className="w-full py-4">
+            Create account
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
